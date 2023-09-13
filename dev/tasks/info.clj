07748cd6e26ec249c4d45901fbd6e6e6e55d0fdf
daslu <daniel.slutsky@gmail.com>
2023-09-13 23:32:46 +0300

    updated info

--- dev/tasks/info.clj
+++ dev/tasks/info.clj
@@ -23,7 +23,7 @@
      (provided (get-in deps [:aliases :plk :extra-deps])))))
 
 (def options
-  {:lib           'djblue/portal
+  {:lib           'daslu/portal-experimental
    :description   "An in official experiment with the Portal tool"
    :version       version
    :url           "https://github.com/daslu/portal"
